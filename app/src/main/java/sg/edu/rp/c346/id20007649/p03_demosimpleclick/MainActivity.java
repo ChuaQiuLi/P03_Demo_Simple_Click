6b03d30aaf1337db604a08071e501d7d4f26153d
20007649 <20007649@rp.edu.sg>
2021-05-07 11:41:40 +0800

    L03 Simple click

--- app/src/main/java/sg/edu/rp/c346/id20007649/p03_demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20007649/p03_demosimpleclick/MainActivity.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    ToggleButton tbn;
 
 
 
@@ -24,5 +26,34 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById((R.id.editTextInput));
+        tbn =findViewById(R.id.toggleButtonEnabled);
+
+        System.out.println("test onCreate");
+
+        //to show what will happen when the user click on to the button
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strRespone = etInput.getText().toString();
+
+                tvDisplay.setText(strRespone);
+
+            }
+        });
+
+        tbn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (tbn.isChecked()){
+                    etInput.setEnabled(true);
+                }
+
+                else{
+                    etInput.setEnabled(false);
+                }
+            }
+        });
     }
 }
